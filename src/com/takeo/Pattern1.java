bc322051602be8ecc4f31931c29eb601022def97
bhipinder <bhupinder@gmail@gmail.com>
2023-12-24 17:40:55 -0500

    CMT

---
+++ src/com/takeo/Pattern1.java
@@ -0,0 +1,15 @@
+package com.takeo;
+
+
+public class Pattern1 {
+	public static void main(String tts[]) {
+		String str="";
+		for(int i=1; i<=5; i++) {
+			str=str+"*";
+			System.out.println(str);	
+		}
+	}
+}
+
+
+		
